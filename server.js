ebe3f03ece99a2e68186548f0aa1f9150d50319f
huongpt1427 <huong.pt@ntu-hn.edu.vn>
2026-03-24 11:27:14 +0700

    Fix: mo ring CORS cho phep localhost

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
 
 app.use(express.json());
